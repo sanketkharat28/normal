dc543f530383e982df87618ec316b739257cfd6e
Sanket Kharat <sanketkharat28@gmail.com>
2025-09-27 23:14:24 +0530

    add colors in tailwind config

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <main>
-      <h1 className='text-5xl text-orange-500 font-bold'>
+      <h1 className='text-5xl text-violet-300 font-bold'>
         Welcome
       </h1>
     </main>
