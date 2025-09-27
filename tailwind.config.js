539c5015740e990e77131e7d7558fa975f0f3d7b
Sanket Kharat <sanketkharat28@gmail.com>
2025-09-27 23:07:09 +0530

    add fonts in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {   
+      fontFamily: {
+        zentry: ['zentry', 'sanf-serif'],
+        general: ['general', 'sanf-serif'],
+        'circular-web': ['circular-web', 'sanf-serif'],
+        'robert-medium': ['robert-medium', 'sanf-serif'],
+        'robert-regular': ['robert-regular', 'sanf-serif'],
+  }},
 
   },
   plugins: [],
