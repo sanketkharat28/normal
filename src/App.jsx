c09d49d0641e71095e6720d874965bfa8d18905b
Sanket Kharat <sanketkharat28@gmail.com>
2025-09-28 15:50:46 +0530

    add small video in the middle with hover

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
+import Hero from './components/Hero'
 
 const App = () => {
   return (
-    <main>
-      <h1 className='text-5xl text-violet-300 font-bold'>
-        Welcome
-      </h1>
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <Hero />
     </main>
   )
 }
